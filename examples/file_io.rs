ab8a78599fc75e5325ad68dbe6f5b5b161b86c31
bkushigian <bkushigian@gmail.com>
2024-10-02 15:03:21 -0700

    Updated file_io

--- examples/file_io.rs
+++ examples/file_io.rs
@@ -1,22 +1,24 @@
 use postflop_solver::*;
+use std::time;
 
 fn main() {
     // see `basic.rs` for the explanation of the following code
 
     let oop_range = "66+,A8s+,A5s-A4s,AJo+,K9s+,KQo,QTs+,JTs,96s+,85s+,75s+,65s,54s";
     let ip_range = "QQ-22,AQs-A2s,ATo+,K5s+,KJo+,Q8s+,J8s+,T7s+,96s+,86s+,75s+,64s+,53s+";
+    let file_save_name = "test_save.pfs";
 
     let card_config = CardConfig {
         range: [oop_range.parse().unwrap(), ip_range.parse().unwrap()],
         flop: flop_from_str("Td9d6h").unwrap(),
-        turn: card_from_str("Qc").unwrap(),
+        turn: NOT_DEALT, //card_from_str("Qc").unwrap(),
         river: NOT_DEALT,
     };
 
     let bet_sizes = BetSizeOptions::try_from(("60%, e, a", "2.5x")).unwrap();
 
     let tree_config = TreeConfig {
-        initial_state: BoardState::Turn,
+        initial_state: BoardState::Flop,
         starting_pot: 200,
         effective_stack: 900,
         rake_rate: 0.0,
@@ -37,22 +39,30 @@ fn main() {
 
     let max_num_iterations = 1000;
     let target_exploitability = game.tree_config().starting_pot as f32 * 0.005;
+    let full_solve_start = time::Instant::now();
     solve(&mut game, max_num_iterations, target_exploitability, true);
+
+    println!(
+        "Full solve: {} seconds",
+        full_solve_start.elapsed().as_secs()
+    );
 
     // save the solved game tree to a file
     // 4th argument is zstd compression level (1-22); requires `zstd` feature to use
-    save_data_to_file(&game, "memo string", "filename.bin", None).unwrap();
+    save_data_to_file(&game, "memo string", file_save_name, None).unwrap();
 
     // load the solved game tree from a file
     // 2nd argument is the maximum memory usage in bytes
     let (mut game2, _memo_string): (PostFlopGame, _) =
-        load_data_from_file("filename.bin", None).unwrap();
+        load_data_from_file(file_save_name, None).unwrap();
 
     // check if the loaded game tree is the same as the original one
     game.cache_normalized_weights();
     game2.cache_normalized_weights();
     assert_eq!(game.equity(0), game2.equity(0));
 
+    println!("\n-----------------------------------------");
+    println!("Saving [Turn Save] to {}", file_save_name);
     // discard information after the river deal when serializing
     // this operation does not lose any information of the game tree itself
     game2.set_target_storage_mode(BoardState::Turn).unwrap();
@@ -69,9 +79,10 @@ fn main() {
 
     // overwrite the file with the truncated game tree
     // game tree constructed from this file cannot access information after the river deal
-    save_data_to_file(&game2, "memo string", "filename.bin", None).unwrap();
+    save_data_to_file(&game2, "memo string", file_save_name, None).unwrap();
 
-    println!("Reloading and Resolving...");
+    println!("Reloading from Turn Save and Resolving...");
+    let turn_solve_start = time::Instant::now();
     let game3 =
         PostFlopGame::hacky_reload_and_resolve(&game2, 100, target_exploitability, true).unwrap();
     for (i, (a, b)) in game2.strategy().iter().zip(game3.strategy()).enumerate() {
@@ -79,27 +90,47 @@ fn main() {
             println!("{i}: Oh no");
         }
     }
+    println!(
+        "Turn solve: {} seconds",
+        turn_solve_start.elapsed().as_secs()
+    );
 
-    // game3.back_to_root();
-    // game3.cache_normalized_weights();
-    // for (a, b) in game2.equity(0).iter().zip(game3.equity(0)) {
-    //     if (a - b).abs() > 0.001 {
-    //         println!("Oh no");
-    //     }
-    // }
-    // game2.back_to_root();
-    // game2.cache_normalized_weights();
+    println!("\n-----------------------------------------");
+    println!("Saving [Flop Save] to {}", file_save_name);
+    // discard information after the river deal when serializing
+    // this operation does not lose any information of the game tree itself
+    game2.set_target_storage_mode(BoardState::Flop).unwrap();
 
-    // for (a, b) in game2
-    //     .expected_values(0)
-    //     .iter()
-    //     .zip(game3.expected_values(0))
-    // {
-    //     if (a - b).abs() > 0.001 {
-    //         println!("{} {} {}", a, b, (a - b).abs());
-    //     }
-    // }
+    // compare the memory usage for serialization
+    println!(
+        "Memory usage of the original game tree: {:.2}MB", // 11.50MB
+        game.target_memory_usage() as f64 / (1024.0 * 1024.0)
+    );
+    println!(
+        "Memory usage of the truncated game tree: {:.2}MB", // 0.79MB
+        game2.target_memory_usage() as f64 / (1024.0 * 1024.0)
+    );
+
+    // overwrite the file with the truncated game tree
+    // game tree constructed from this file cannot access information after the river deal
+    save_data_to_file(&game2, "memo string", file_save_name, None).unwrap();
+
+    println!("Reloading from River Save and Resolving...");
+    let river_solve_start = time::Instant::now();
+    let game3 =
+        PostFlopGame::hacky_reload_and_resolve(&game2, 100, target_exploitability, true).unwrap();
+
+    println!(
+        "River solve: {} seconds",
+        river_solve_start.elapsed().as_secs()
+    );
+
+    for (i, (a, b)) in game2.strategy().iter().zip(game3.strategy()).enumerate() {
+        if (a - b).abs() > 0.001 {
+            println!("{i}: Oh no");
+        }
+    }
 
     // delete the file
-    std::fs::remove_file("filename.bin").unwrap();
+    std::fs::remove_file(file_save_name).unwrap();
 }
