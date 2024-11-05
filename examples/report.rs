ce07ce8bafab882c43ba0ba49e11f4fecff953a1
Jacob Van Geffen <jsvangeffen@gmail.com>
2024-11-05 13:58:46 -0600

    Take lines as input when initializing agg report tree

--- examples/report.rs
+++ examples/report.rs
@@ -1,8 +1,8 @@
 use postflop_solver::*;
-use utils::batch::report::AggActionTree;
+use utils::batch::report::{generate_all_lines, AggActionTree};
 
 // Uncomment if reloading from previous game saves
-use utils::flop_helper::flop_to_string;
+// use utils::flop_helper::flop_to_string;
 
 fn main() {
     let oop_range = "66+,A8s+,A5s-A4s,AJo+,K9s+,KQo,QTs+,JTs,96s+,85s+,75s+,65s,54s";
@@ -32,30 +32,32 @@ fn main() {
         merging_threshold: 0.1,
     };
 
-    let mut report_tree = AggActionTree::init_root();
+    let lines = generate_all_lines(tree_config.clone()).unwrap();
+
+    let mut report_tree = AggActionTree::init_root(lines, tree_config.clone()).unwrap();
 
     for (i, &flop) in flops.iter().enumerate() {
-        // let card_config = CardConfig {
-        //     range: [oop_range.parse().unwrap(), ip_range.parse().unwrap()],
-        //     flop,
-        //     turn: NOT_DEALT,
-        //     river: NOT_DEALT,
-        // };
+        let card_config = CardConfig {
+            range: [oop_range.parse().unwrap(), ip_range.parse().unwrap()],
+            flop,
+            turn: NOT_DEALT,
+            river: NOT_DEALT,
+        };
 
-        // let action_tree = ActionTree::new(tree_config.clone()).unwrap();
-        // let mut game = PostFlopGame::with_config(card_config, action_tree).unwrap();
-        // game.allocate_memory(false);
+        let action_tree = ActionTree::new(tree_config.clone()).unwrap();
+        let mut game = PostFlopGame::with_config(card_config, action_tree).unwrap();
+        game.allocate_memory(false);
 
-        // let max_num_iterations = 1000;
-        // let target_exploitability = game.tree_config().starting_pot as f32 * 0.005;
-        // solve(&mut game, max_num_iterations, target_exploitability, true);
+        let max_num_iterations = 1000;
+        let target_exploitability = game.tree_config().starting_pot as f32 * 0.005;
+        solve(&mut game, max_num_iterations, target_exploitability, true);
 
         // Uncomment to reload previously-saved game
         // (Should also comment out above solving code)
-        let (mut game, _): (PostFlopGame, _) =
-            load_data_from_file(format!("flops/{}.bin", flop_to_string(&flop)), None).unwrap();
+        // let (mut game, _): (PostFlopGame, _) =
+        //     load_data_from_file(format!("flops/{}.bin", flop_to_string(&flop)), None).unwrap();
 
-        report_tree.update(&mut game, &flop);
+        report_tree.update_report_for_game(&mut game, &flop);
 
         // Log progress
         if (i + 1) % 10 == 0 {
